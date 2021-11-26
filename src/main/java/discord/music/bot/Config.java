bbb3cc8f49496b9708264aeaa233445d8d96d7b8
disornthitikornkovit <dsorn2@gmail.com>
2021-11-26 21:44:43 +0700

    add procfile

--- src/main/java/discord/music/bot/Config.java
+++ src/main/java/discord/music/bot/Config.java
@@ -3,8 +3,8 @@ package discord.music.bot;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class Config {
-    private static final Dotenv dotenv = Dotenv.load();
+//    private static final Dotenv dotenv = Dotenv.load();
     public static String get(String key) {
-        return dotenv.get(key.toUpperCase());
+        return System.getenv(key.toUpperCase());
     }
 }
